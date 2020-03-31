2d710e69df8da3202d5b2af20babf49511afe48f
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-31 22:11:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,10 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -11,4 +15,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
 end
